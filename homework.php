308dfaf652fd2ce41eaf51860755583e90b9979e
kamana <kamana>
2019-09-22 16:09:47 +0545

    third project

--- homework.php
+++ homework.php
@@ -1,9 +1,8 @@
 <!doctype html>
 <html>
-    <head>
-        <title>homework</title>
+<head>
+    <title>homework</title>
          <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-
             <style>
                 .name p{
                     background-color:black;
@@ -11,9 +10,7 @@
                     color:white;
                     font-weight:bolder;
                     font-size:15pt;
-                    
-                    
-              }
+                    }
                 .home p{
                     background-color:green;
                     border:150px solid green;
@@ -21,7 +18,7 @@
                     font_weight:bolder;
                     margin-top:-25px;
                     font-size:50pt;
-             }
+                    }
                 .amazing p{
                     background-color:grey;
                     border:15px solid grey;
@@ -29,65 +26,64 @@
                     font-size:15pt;
                     margin-top:-70px;
                     text-align:center;
-             }
+                    }
                 .icons p{
                     font-size:30pt;
                     margin-left:70px;
-             }
+                    }
                 .text p{
                     font-size:10pt;
                     margin-left:25px;
                     margin-top:-5px;
-            }
+                    }
                 .service p{
                     font-size:26pt;
                     margin-left:420px;
                     margin-top:-164px;
-                    
-             }
+                    }
              .company p{
                  margin-left:404px;
                  margin-top:-22px;
                  font-size:9pt;
-             }
+                 }
              .faces p{
                  margin-left:768px;
                  margin-top:-158px;
                  font-size:27pt;
-             }
+                 }
              .face p{
                  margin-left:763px;
                  margin-top:-17px;
                  font-size:9pt;
-             }
+                 }
              .del p{
                  margin-left:1116px;
                  margin-top:-155px;
                  font-size:26pt;
-             }
+                 }
              .delete p{
                  margin-left:1101px;
                  margin-top:-23px;
-                 font-size:9pt;  
-             }
+                 font-size:9pt;
+                 }
              .title p{
                  font-size:23pt;
                  background-color:grey;
                  border:80px solid grey;
                  text-align:center;
-             }
+                 }
              .paragraph p{
                  margin-left:515px;
                  margin-top:-104px;
                  font-size:9pt;
-             }
+                 }
              .gallery p{
                  text-align:center;
                  margin-top:-28px;
                  font-size:23pt;
                  padding:89px;
                  margin-left:5px;
-             } 
+                 } 
              .color-box p{
                  background-color:green;
                  width:274px;
@@ -96,8 +92,7 @@
                  font-size:32px;
                  text-align:center;
                  margin-top:-119px;
-                 
-             }
+                 }
              .color-box-yellow p{
                  background-color:yellow;
                  width:274px;
@@ -105,8 +100,8 @@
                  margin-left:410px;
                  font-size:32px;
                  text-align:center;
-                 margin-top:-222px; 
-             }
+                 margin-top:-222px;
+                 }
              .color-box-red p{
                  background-color:red;
                  width:274px;
@@ -114,8 +109,8 @@
                  margin-left:700px;
                  font-size:32px;
                  text-align:center;
-                 margin-top:-222px; 
-             }
+                 margin-top:-222px;
+                 }
              .color-box-blue p{
                  background-color:blue;
                  width:274px;
@@ -123,8 +118,8 @@
                  margin-left:1000px;
                  font-size:32px;
                  text-align:center;
-                 margin-top:-222px;  
-             }
+                 margin-top:-222px;
+                 }
              .responsive p{
                  background-color:grey;
                  border:50px solod grey;
@@ -136,107 +131,112 @@
                  color:white;
                  margin-left:5px;
                  text-align:center;
-             }
+                 }
              .block p{
                  font-size:11pt;
                  margin-top:-300px;
-
-             }
-
-    
-
-
- </style>
+                 }
+            </style>
  </head>               
  <body>
  <div class="name">
- <p>sitelogo</p>
-    </div>
+    <p>sitelogo</p>
+</div>
  <div class="home">
      <p>Header image</p>
-    </div>
+</div>
 <div class="amazing">
     <p>Amazing responsive carsoule</p>
 </div>
 <div class="icons">
-<p>About</p>
-        <i class="material-icons"style="font-size:40pt;margin-left:75px;margin-top:-40px;">home</i>
+    <p>About</p>
+        <i class="material-icons"style="font-size:40pt;margin-left:75px;margin-top:-40px;">home
+        </i>
 </div>
 <div class="text">
     <p>Separation of formatting and<br>
+    content also makes it feasible<br>
+    to present the same markup page<br>
+    in different styles for different
+    rendering .
+    </p>
+</div>
+<div class="service">
+    <p>Service</p>
+        <i class="material-icons"style="font-size:40pt;margin-left:444px;margin-top:-34px;">search
+        </i>
+</div>
+<div class="company">
+    <p> <p>Separation of formatting and<br>
      content also makes it feasible<br>
-      to present the same markup page<br>
-       in different styles for different
-        rendering .</p>
-    </div>
-    <div class="service">
-        <p>Service</p>
-        <i class="material-icons"style="font-size:40pt;margin-left:444px;margin-top:-34px;">search</i>
-    </div>
-    <div class="company">
-        <p> <p>Separation of formatting and<br>
-     content also makes it feasible<br>
-      to present the same markup page<br>
-       in different styles for different
-        rendering .</p>
-    </div>
-    <div class="faces">
+     to present the same markup page<br>
+     in different styles for different
+     rendering .
+    </p>
+</div>
+<div class="faces">
         <p>company</p>
-        <i class="material-icons"style="font-size:40pt;margin-left:810px;margin-top:-39px;">face</i>
-    </div>
-    <div class="face">
-        <p> <p>Separation of formatting and<br>
-     content also makes it feasible<br>
-      to present the same markup page<br>
-       in different styles for different
-        rendering .</p>  
-    </div>
-    <div class="del">
+            <i class="material-icons"style="font-size:40pt;margin-left:810px;margin-top:-39px;">face
+            </i>
+</div>
+<div class="face">
+        <p>Separation of formatting and<br>
+         content also makes it feasible<br>
+         to present the same markup page<br>
+         in different styles for different
+         rendering .
+        </p>  
+</div>
+<div class="del">
         <p>contact</p>
-        <i class="material-icons"style="font-size:40pt;margin-left:1147px;margin-top:-39px;">search</i>
-    </div>
-    <div class="delete">
-        <p> <p>Separation of formatting and<br>
-     content also makes it feasible<br>
-      to present the same markup page<br>
-       in different styles for different
-        rendering .</p>  
-    </div> 
-    <div class="title">
-    <p>Amazing title</p>
-    </div>
-    <div class="paragraph">
-     <p>Separation of formatting and content also makes it feasible<br>
-      to present the same markup page in different styles for different
-        rendering .</p> 
-        <div class="gallery">
-            <p>Gallery</p>        
-            </div>
-            <div class="color-box">
-            <p>Gallery image</p>
-            </div>
-            <div class="color-box-yellow">
-            <p>Gallery image</p>
-            </div>
-            <div class="color-box-red">
-            <p>Gallery image</p>
-            </div>
-            <div class="color-box-blue">
-            <p>Gallery image</p>
-            </div>
-            <div class="responsive">
-            <p>Amazing responsive template</p>
-            </div>
-            <div class="block">
-        <p> <p>Separation of formatting and<br>
-     content also makes it feasible<br>
-      to present the same markup page<br>
-       in different styles for different
-        rendering .</p>  
-    </div> 
+            <i class="material-icons"style="font-size:40pt;margin-left:1147px;margin-top:-39px;">search
+            </i>
+</div>
+<div class="delete">
+        <p>Separation of formatting and<br>
+        content also makes it feasible<br>
+        to present the same markup page<br>
+        in different styles for different
+        rendering .
+        </p>  
+</div> 
+<div class="title">
+        <p>Amazing title</p>
+</div>
+<div class="paragraph">
+        <p>Separation of formatting and content also makes it feasible<br>
+        to present the same markup page in different styles for different
+        rendering .
+        </p> 
+<div class="gallery">
+        <p>Gallery</p>        
+</div>
+<div class="color-box">
+        <p>Gallery image</p>
+</div>
+<div class="color-box-yellow">
+        <p>Gallery image</p>
+</div>
+<div class="color-box-red">
+        <p>Gallery image</p>
+</div>
+<div class="color-box-blue">
+        <p>Gallery image</p>
+</div>
+<div class="responsive">
+        <p>Amazing responsive template</p>
+</div>
+<div class="block">
+        <p>Separation of formatting and<br>
+         content also makes it feasible<br>
+         to present the same markup page<br>
+         in different styles for different
+         rendering .
+        </p>  
+</div> 
 
-            </body>
- </html>
+</body>
+</html>
  
                 
             
