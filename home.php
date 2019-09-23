ed6f8c48a898bc188d5b35e3c653c6e2a79432a7
kamana-ghimire <47885944+kamana-ghimire@users.noreply.github.com>
2019-09-23 11:09:27 +0545

    Delete home.php

--- home.php
+++
@@ -1,9 +0,0 @@
-<html>
-     <title></title>
-      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-       <body>
-            <i class="material-icons">home</i>
-             <i class="material-icons">search</i>
-              <i class="material-icons">face</i>
-             </body>
-              </html>
