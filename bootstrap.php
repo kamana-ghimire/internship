8b406064108d5740a0f58df8058da8fda5f21adb
kamana <kamana>
2019-12-23 16:28:58 +0545

    bootstrap

---
+++ bootstrap.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+ 
+<div class="container">
+  <h2>Title</h2>
+  <h3 style="margin-top:-45px;margin-left:17rem;"><U>USDAUD</U></h3>
+  <div class="card" style="width:400px">
+    <img class="card-img-top" src="picture/portfolio4.jpg" alt="Card image" style="width:100%">
+    </div>
+    <div class="card" style="width:5rem;margin-top:14px;">
+    <p class="card-title">USDAUD</p>
+    </div>
+    <div class="card" style="width: 5rem;margin-left: 20rem;margin-top: -39px;">
+    <p class="card-title">1.3954</p>
+    </div>
+    <p style="margin-top:10px;"> A list of three to five people who can
+     verify professional<br> qualifications.
+      Recommendations of<br> professi references
+       include faculty members, internship<br>supervisors,
+        employment supervisors and supervisors other<br>
+         activities such as community service projects.
+         </p>
+    <p><b>33 likes</b></p>
+    <div class="per" style="margin-top:-40px;margin-left:19rem;">
+    <p><b>2 comments</b></p>
+    </div>
+</div>
+  
+
+</body>
+</html>
