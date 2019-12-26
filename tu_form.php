baf4302d1b47d8777784f60e2cb5d3a6a06e24d2
kamana <kamana>
2019-12-26 16:53:40 +0545

    form

---
+++ tu_form.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" 
+    integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Satisfy&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+    crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<style>
+ .card {
+    height: 3rem;
+    margin-top: 5rem;
+    background-color: ghostwhite;
+ }
+ .button {
+    width: 9rem;
+    margin-left: 2px;
+ }
+ .button2 {
+    width: 9rem;
+    margin-left: 7rem;
+    margin-top:-26px;
+ }
+ .card {
+     margin-top:0px;
+
+ }
+ </style>
+<body>
+<div class="container">
+    <div class="card">
+        <div class="button">
+        <button type="button">General</button>
+            <div class="button2">
+            <button type="button">documents</button>
+            </div>
+        </div>
+    </div>
+    <div class="card">
+    </div>
+    <form>
+    <div class="card" style="">
+  <div class="form-row">
+    <div class="col">
+      <input type="text" class="form-control" placeholder="First name">
+    </div>
+    <div class="col">
+      <input type="text" class="form-control" placeholder="Last name">
+    </div>
+  </div>
+</form>  
+                    
---
+++ tu_form2.php
@@ -0,0 +1,317 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" 
+    integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Satisfy&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+    crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<style>
+.card {
+    width: 106rem;
+    margin-top:2rem;
+    margin-left: 8rem
+}
+.row2 {
+    height:3rem;
+    background-color: #ccd9ff
+}
+</style>
+<body>
+<div class="card">
+  <div class="card-header">
+  <button type="button">General</button>
+  <button type="button">Document</button>
+</div>
+<div class="row2">
+  <ul class="list-group list-group-flush">
+</div>
+<div class="row3">
+    <li class="list-group-item">
+    <form>
+    <div class="form-row mb-4">
+        <div class="col">
+            <!-- First name -->
+            <input type="text" id="defaultRegisterFormFirstName" class="form-control" placeholder="First name" style="width:20rem;margin-top:2rem; margin-left:4px;">
+        </div>
+        <div class="col">
+            <!-- Last name -->
+            <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="Last name" style="width:20rem;margin-left: 6rem;margin-top: 22px;">
+        </div>
+        <div class="col">
+            <!-- Last name -->
+            <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="date" style="width:20rem;margin-left: 12rem;margin-top: 22px;">
+        </div>
+    </div>
+</form>
+ </li>
+</div>
+    <li class="list-group-item" style="height: 9rem"></li>
+  </ul>
+</div>
+</div>
+<div class="card">
+<div class="row2">
+  <ul class="list-group list-group-flush">
+</div>
+<div class="row3">
+    <li class="list-group-item">
+    <form>
+    <div class="form-row mb-4">
+        <div class="col">
+            <!-- First name -->
+            <input type="text" id="defaultRegisterFormFirstName" class="form-control" placeholder="First name" style="width:20rem;margin-top:2rem; margin-left:4px;">
+        </div>
+        <div class="col">
+            <!-- Last name -->
+            <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="Last name" style="width:20rem;margin-left: 1rem;margin-top: 22px;">
+        </div>
+        <div class="col">
+            <!-- Last name -->
+            <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="date" style="width:12rem;margin-left:12rem;margin-top: 22px;">
+        </div>
+        <div class="col">
+            <!-- Last name -->
+            <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="time" style="width:13rem;margin-left: 12rem;margin-top: 22px;">
+        </div>
+    </div>
+    <input type="checkbox" id="defaultRegisterFormLastName" style="margin-top:2rem";>
+    <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="" style="width:11rem;margin-left:26rem;margin-top:-6px;height:3rem;">
+    <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="" style="width:8rem;margin-left: 12rem;margin-top:33px;">
+    <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="" style="width:8rem;margin-left:24rem;margin-top:-34px;">
+    <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="" style="width:8rem;margin-left:36rem;margin-top:-34px;">
+    <input type="checkbox" style=" margin-top: -11rem;margin-left: 47rem;">
+    <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="" style="width:8rem;margin-left:52rem;margin-top:-51px;">
+    <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="" style="width:8rem;margin-left:64rem;margin-top:-34px;">
+    <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="" style="width:8rem;margin-left:79rem;margin-top:-34px;">
+</form>
+ </li>
+</div>
+  </ul>
+</div>
+</div>
+<div class="card">
+<div class="row2">
+  <ul class="list-group list-group-flush">
+</div>
+<div class="row3">
+    <li class="list-group-item" style="height:6rem;">
+    <input type="checkbox" id="defaultRegisterFormLastName" style="margin-top:2rem";>
+</div>
+</div>
+<div class="card">
+<div class="row2">
+  <ul class="list-group list-group-flush">
+</div>
+<div class="row3">
+    <li class="list-group-item">
+    <form>
+    <div class="form-row mb-4">
+        <div class="col">
+            <!-- First name -->
+            <input type="text" id="defaultRegisterFormFirstName" class="form-control" placeholder="First name" style="width:20rem;margin-top:2rem; margin-left:4px;">
+        </div>
+        <div class="col">
+            <!-- Last name -->
+            <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="Last name" style="width:20rem;margin-left: 1rem;margin-top: 22px;">
+        </div>
+        <div class="col">
+            <!-- Last name -->
+            <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="date" style="width:12rem;margin-left:12rem;margin-top: 22px;">
+        </div>
+        <div class="col">
+            <!-- Last name -->
+            <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="time" style="width:13rem;margin-left: 12rem;margin-top: 22px;">
+        </div>
+        <div class="col">
+            <!-- Last name -->
+            <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="time" style="width:20rem;margin-top: 27px;">
+        </div>
+    </div>
+</div>
+</div>    
+<div class="card">
+<div class="row2">
+  <ul class="list-group list-group-flush">
+</div>
+<div class="row3">
+    <li class="list-group-item">
+    <form>
+    <div class="form-row mb-4">
+        <div class="col">
+            <!-- First name -->
+            <input type="text" id="defaultRegisterFormFirstName" class="form-control" placeholder="First name" style="width:20rem;margin-top:2rem; margin-left:4px;">
+        </div>
+        <div class="col">
+            <!-- Last name -->
+            <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="Last name" style="width:20rem;margin-left: 1rem;margin-top: 22px;">
+        </div>
+        <div class="col">
+            <!-- Last name -->
+            <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="" style="width:12rem;margin-left:12rem;margin-top: 22px;">
+        </div>
+        <div class="col">
+            <!-- Last name -->
+            <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="" style="width:13rem;margin-left: 12rem;margin-top: 22px;">
+        </div>
+        <div class="col">
+            <!-- Last name -->
+            <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="" style="width:20rem;margin-top: 27px;">
+        </div>
+        <div class="col">
+            <!-- Last name -->
+            <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="" style="width:20rem;margin-top: 27px;margin-left:6rem;">
+        </div>
+        <div class="col">
+            <!-- Last name -->
+            <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="" style="width:20rem;margin-top: 27px;margin-left:9rem;">
+        </div>
+        <div class="col">
+            <!-- Last name -->
+            <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="" style="width:20rem;margin-top:9rem;margin-left:-78rem;">
+        </div>
+        <div class="col">
+            <!-- Last name -->
+            <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="" style="width:20rem;margin-top:92px;margin-left:-64rem;">
+        </div>
+    </div>
+</div>
+</div>
+<div class="card">
+<div class="row2">
+  <ul class="list-group list-group-flush">
+</div>
+<div class="row3">
+    <li class="list-group-item">
+    <form>
+    <div class="form-row mb-4">
+        <div class="col">
+            <!-- First name -->
+            <input type="text" id="defaultRegisterFormFirstName" class="form-control" placeholder="First name" style="width:20rem;margin-top:2rem; margin-left:4px;">
+        </div>
+        <div class="col">
+            <!-- Last name -->
+            <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="Last name" style="width:20rem;margin-left: 1rem;margin-top: 22px;">
+        </div>
+        <div class="col">
+            <!-- Last name -->
+            <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="" style="width:20rem;margin-left:7rem;margin-top: 22px;">
+        </div>   
+    </div>
+</div>
+</div>
+<div class="card">
+<div class="row2">
+  <ul class="list-group list-group-flush">
+</div>
+<div class="row3">
+    <li class="list-group-item">
+    <form>
+    <div class="form-row mb-4">
+        <div class="col">
+            <!-- First name -->
+            <input type="text" id="defaultRegisterFormFirstName" class="form-control" placeholder="First name" style="width:20rem;margin-top:2rem; margin-left:4px;">
+        </div>
+        <div class="col">
+            <!-- Last name -->
+            <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="Last name" style="width:20rem;margin-left: 14rem;margin-top: 22px;">
+        </div>
+        <div class="col">
+            <!-- Last name -->
+            <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="" style="width:20rem;margin-left:20rem;margin-top: 22px;">
+        </div>
+        <div class="col">
+            <!-- First name -->
+            <input type="text" id="defaultRegisterFormFirstName" class="form-control" placeholder="First name" style="width:20rem;margin-top:9rem; margin-left:-97rem;">
+        </div>
+        <div class="col">
+            <!-- Last name -->
+            <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="Last name" style="width:20rem;margin-left: 35rem;margin-top:-34px;">
+        </div>
+        <div class="col">
+            <!-- Last name -->
+            <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="" style="width:20rem;margin-left:20rem;margin-top:-29px;">
+        </div> 
+        <div class="col">
+            <!-- Last name -->
+            <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="" style="width:20rem;margin-left:2px;margin-top:28px;">
+        </div> 
+        <div class="col">
+            <!-- Last name -->
+            <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="" style="width:20rem;margin-left:2px;margin-top:29px;">
+        </div> 
+        <div class="col">
+            <!-- Last name -->
+            <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="" style="width:20rem;margin-left:70px;margin-top:34px;">
+        </div>
+    </div>
+</div>
+</div>
+<div class="card">
+<div class="row2">
+  <ul class="list-group list-group-flush">
+</div>
+<div class="row3">
+    <li class="list-group-item">
+    <input type="checkbox" id="defaultRegisterFormLastName" >
+    <input type="checkbox" id="defaultRegisterFormLastName" style="margin-left:7rem;">
+    <input type="checkbox" id="defaultRegisterFormLastName" style="margin-left:10rem;">
+    <input type="checkbox" id="defaultRegisterFormLastName" style="margin-left:13rem;">
+    <input type="checkbox" id="defaultRegisterFormLastName" style="margin-left:16rem;">
+    <input type="checkbox" id="defaultRegisterFormLastName" style="margin-left:19rem;">
+    <input type="checkbox" id="defaultRegisterFormLastName" style="margin-left:21rem;">
+</div>
+</div>
+<div class="card">
+<div class="row2">
+  <ul class="list-group list-group-flush">
+</div>
+<div class="row3">
+<ol>
+  <li></li>
+  <li></li>
+</ol>
+</div>
+<input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="" style="width:14rem;margin-left: 19px;-top: 6px;height: 21px">
+</div>
+<div class="card">
+<div class="row2">
+  <ul class="list-group list-group-flush">
+</div>
+<div class="row3">
+<ol>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+</ol>
+</div>
+<input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="" style="width:14rem;margin-left: 19px;-top: 6px;height: 21px">
+<input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="" style="width:8rem;margin-left:222px;-top: 6px;height:29px;margin-top:23px">
+<input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="" style="width:8rem;margin-left:384px;-top: 6px;height:29px;margin-top:-29px">
+<input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="" style="width:8rem;margin-left:542px;-top: 6px;height:29px;margin-top:-29px">
+<input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="" style="width:8rem;margin-left:714px;-top: 6px;height:29px;margin-top:-29px">
+</div>
+<div class="card">
+<div class="row2">
+  <ul class="list-group list-group-flush">
+</div>
+<div class="row3">
+  <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="" style="width:15rem;margin-left:3px;-top: 6px;height:29px;margin-top:25px">
+</div>
+        
+        
+ 
+
+    
+         
+
+
